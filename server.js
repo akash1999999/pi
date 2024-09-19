0f4333d3ce290779dd85031752b1bb7d344ff929
akash1999999 <akashsinghinz@gmail.com>
2024-09-19 14:06:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -90,18 +90,15 @@ function setcrash() {
      
      if(betamount==0){
       finalcrash =Math.floor(Math.random() * 6) + 2;
-      //finalcrash =(Math.random() * 0.5 + 1).toFixed(1);
       //console.log('finalcrash0');
       //console.log(finalcrash,betamount);
       repeatupdate(200);
      }else if(betamount<=100){
-      //finalcrash =Math.floor(Math.random() * 6) + 1; 
       finalcrash =(Math.random() * 0.5 + 1).toFixed(2); 
       //console.log('finalcrash100');
       //console.log(finalcrash,betamount);
       repeatupdate(300);
      }else{
-      //finalcrash =Math.floor(Math.random() * 6) + 1;
       finalcrash =(Math.random()* 0.5  + 1).toFixed(2); 
       //console.log('finalcrash12');
       //console.log(finalcrash,betamount);
